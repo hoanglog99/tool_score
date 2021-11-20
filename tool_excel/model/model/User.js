f9853ca72414d55e7a733e83720756b612586a10
hoanglog99 <93933024+hoanglog99@users.noreply.github.com>
2021-11-20 20:57:15 +0700

    Add files via upload

---
+++ tool_excel/model/model/User.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    email: { type: String, unique: true, required: true, trim: true },
+    password: { type: String, required: true, trim: true, minlength: 6 },
+});
+
+module.exports = mongoose.model('User', userSchema)
